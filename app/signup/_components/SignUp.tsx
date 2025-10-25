05ba18855e98e7d53a2e7592f0e2cf9b83e60861
Shayokh <shayokhshuvro14@gmail.com>
2025-10-25 20:14:09 +0600

    add proper integration with Tanstack form, complete sign in and sign up UI

--- app/signup/_components/SignUp.tsx
+++ app/signup/_components/SignUp.tsx
@@ -1,99 +1,192 @@
+/** biome-ignore-all lint/correctness/noChildrenProp: <required for Tanstack form> */
 "use client";
 
+import { type AnyFieldApi, useForm } from "@tanstack/react-form";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import eye from "@/public/eye.svg";
-import eye_closed from "@/public/eye-closed.svg";
+import { z } from "zod";
 import google from "@/public/google.svg";
 import logo from "@/public/logo.svg";
 
-export function SignUp() {
-  return (
-    <section className="flex flex-col items-center justify-center h-svh">
-      <div className="flex flex-col items-center max-w-64">
-        <Image src={logo} alt="logo" className="w-26" />
+const signUpSchema = z
+  .object({
+    name: z
+      .string()
+      .max(50, { message: "Name cannot be longer than 50 characters." })
+      .nonempty({ message: "Please enter your name." }),
+    email: z
+      .email({ message: "Please enter a valid email address." })
+      .max(40, { message: "Email cannot be longer than 40 characters." })
+      .nonempty({ message: "Please enter your email address." }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long." })
+      .max(30, { message: "Password cannot be longer than 30 characters." })
+      .nonempty({ message: "Please enter a password." })
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+        "Ensure at least one uppercase letter, one lowercase letter, and a number in your password.",
+      ),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Please confirm your password." })
+      .max(30, {
+        message: "Confirmation password cannot be longer than 30 characters.",
+      })
+      .nonempty({ message: "Please confirm your password." }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match. Please try again.",
+    path: ["confirmPassword"],
+  });
 
-        <p className="text-xl text-neutral-300 font-medium mt-[1rem]">
-          Create a new account
+export function SignUp() {
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+
+    validators: {
+      onChange: signUpSchema,
+    },
+
+    onSubmit: ({ value }) => {
+      console.log(
+        value.name,
+        value.email,
+        value.password,
+        value.confirmPassword,
+      );
+    },
+  });
+  return (
+    <section className="flex flex-col items-center justify-center h-svh font-sans">
+      <div className="flex flex-col items-center max-w-[15rem]">
+        <Image src={logo} alt="icon" className="w-20 xs:w-25 md:w-30" />
+
+        <p className="text-sm sm:text-base text-neutral-800 mt-2">
+          Let's create your account.
         </p>
 
-        {/* action={handleGoogleAuth} */}
         <form className="w-full">
           <button
-            type="submit"
-            className="flex gap-x-3 justify-center items-center text-nowrap w-full bg-neutral-200 text-neutral-800 font-medium mt-6 py-2 rounded-sm hover:cursor-pointer select-none"
+            type="button"
+            className="flex gap-x-3 justify-center items-center text-nowrap w-full border border-preply-green text-neutral-800 font-medium mt-6 py-2 rounded-sm hover:cursor-pointer select-none"
           >
             <Image src={google} alt="icon" className="size-5" />
             <span className="text-sm">Continue with Google</span>
           </button>
         </form>
-        <div className="flex items-center gap-x-3 w-full mt-2">
-          <div className="h-[0.5px] w-full bg-neutral-800" />
-          <p className="text-neutral-500 text-sm text-nowrap">or use email</p>
-          <div className="h-[0.5px] w-full bg-neutral-800" />
+
+        <div className="flex items-center gap-x-3 w-full mt-3">
+          <div className="h-[0.5px] w-full bg-neutral-200" />
+          <p className="text-neutral-800 text-sm text-nowrap">or use email</p>
+          <div className="h-[0.5px] w-full bg-neutral-200" />
         </div>
+
         <form
-          className="flex flex-col w-full gap-y-2 mt-2 text-sm"
-          //   onSubmit={handleSubmit(onCredentialSubmitLogic)}
+          className="flex flex-col w-full gap-y-2 mt-3 text-sm"
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            form.handleSubmit();
+          }}
         >
-          <input
-            // {...register("name")}
-            type="text"
+          <form.Field
             name="name"
-            placeholder="John Doe"
-            className="px-4 py-2 rounded-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500"
+            children={(field) => (
+              <>
+                <input
+                  type="text"
+                  name="name"
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  placeholder="John Doe"
+                  className="px-4 py-2 rounded-lg border border-neutral-300 focus:outline-none focus:border-preply-green placeholder-neutral-400"
+                />
+
+                <ErrorInfo field={field} />
+              </>
+            )}
           />
 
-          {/* {errors.name && (
-            <p className="text-red-500 text-sm">{errors.name.message}</p>
-          )} */}
-
-          <input
-            // {...register("email")}
-            type="text"
+          <form.Field
             name="email"
-            placeholder="john@example.com"
-            className="px-4 py-2 rounded-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500"
+            children={(field) => (
+              <>
+                <input
+                  type="text"
+                  name="email"
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  placeholder="john@example.com"
+                  className="px-4 py-2 rounded-lg border border-neutral-300 focus:outline-none focus:border-preply-green placeholder-neutral-400"
+                />
+
+                <ErrorInfo field={field} />
+              </>
+            )}
           />
 
-          {/* {errors.email && (
-            <p className="text-red-500 text-sm">{errors.email.message}</p>
-          )} */}
+          <form.Field
+            name="password"
+            children={(field) => (
+              <>
+                <input
+                  type="password"
+                  name="password"
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  placeholder="Create a password"
+                  className="px-4 py-2 rounded-lg border border-neutral-300 focus:outline-none focus:border-preply-green placeholder-neutral-400"
+                />
 
-          {/* <PasswordField errors={errors} register={register} /> */}
+                <ErrorInfo field={field} />
+              </>
+            )}
+          />
 
-          <input
-            // {...register("confirmPassword")}
-            type="password"
+          <form.Field
             name="confirmPassword"
-            placeholder="Confirm your password"
-            className="px-4 py-2 rounded-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500 w-full"
+            children={(field) => (
+              <>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  placeholder="Confirm your password"
+                  className="px-4 py-2 rounded-lg border border-neutral-300 focus:outline-none focus:border-preply-green placeholder-neutral-400"
+                />
+
+                <ErrorInfo field={field} />
+              </>
+            )}
           />
-          {/* 
-          {errors.confirmPassword && (
-            <p className="text-red-500 text-sm">
-              {errors.confirmPassword.message}
-            </p>
-          )}
 
-          {errors.root && (
-            <p className="text-red-500 text-sm">{errors.root.message}</p>
-          )} */}
-
-          <button
-            type="submit"
-            // disabled={isSubmitting}
-            className="px-3 py-2 hover:cursor-pointer bg-neutral-800 hover:bg-neutral-700 rounded-lg text-sm text-neutral-200 font-medium transition-colors duration-150 disabled:cursor-not-allowed disabled:bg-neutral-900"
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isValidating]}
           >
-            Placeholder
-            {/* {isSubmitting ? "Hashing..." : "Sign up"} */}
-          </button>
+            {([canSubmit, isValidating]) => (
+              <button
+                type="submit"
+                onClick={form.handleSubmit}
+                disabled={!canSubmit || isValidating}
+                className="px-3 py-2 hover:cursor-pointer bg-neutral-800 rounded-lg text-sm text-neutral-200 font-medium transition-colors duration-150 disabled:cursor-not-allowed disabled:bg-neutral-900"
+              >
+                {isValidating ? "Hashing..." : "Sign up"}
+              </button>
+            )}
+          </form.Subscribe>
         </form>
-        <p className="text-neutral-500 text-sm text-nowrap mt-4">
+
+        <p className="text-sm text-nowrap mt-4">
           Already have an account?{" "}
-          <Link href="/login" className="text-neutral-300 underline">
-            Login
+          <Link href="/signin" className="underline">
+            Sign in
           </Link>
         </p>
       </div>
@@ -101,41 +194,17 @@ export function SignUp() {
   );
 }
 
-function PasswordField() {
-  const [isPasswordShown, setIsPasswordShown] = useState(false);
-
+function ErrorInfo({ field }: { field: AnyFieldApi }) {
   return (
     <>
-      <div className="w-full flex">
-        <input
-          type={isPasswordShown ? "text" : "password"}
-          name="password"
-          placeholder="Enter your password"
-          className="px-4 py-2 rounded-l-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500 w-full"
-        />
-
-        <button
-          type="button"
-          className="p-2 border border-l-0 border-neutral-600 bg-neutral-800 rounded-r-lg hover:cursor-pointer"
-          onClick={() => setIsPasswordShown(!isPasswordShown)}
-        >
-          {/* biome-ignore lint/performance/noImgElement: <> */}
-          <img
-            src={isPasswordShown ? eye_closed : eye}
-            alt={isPasswordShown ? "Hide password" : "Show password"}
-          />
-
-          {isPasswordShown ? (
-            <span className="sr-only">Hide password</span>
-          ) : (
-            <span className="sr-only">Show password</span>
-          )}
-        </button>
-      </div>
-
-      {/* {errors.password && (
-        <p className="text-red-500 text-sm">{errors.password.message}</p>
-      )} */}
+      {field.state.meta.isTouched &&
+      !field.state.meta.isValid &&
+      field.state.meta.errors.length > 0 ? (
+        <p className="text-red-500 text-sm">
+          {field.state.meta.errors[0].message}
+        </p>
+      ) : null}
+      {field.state.meta.isValidating ? "Validating..." : null}
     </>
   );
 }
