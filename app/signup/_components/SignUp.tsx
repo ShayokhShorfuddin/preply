9c076f3f0bf8dc55457c5514360b440621341200
Shayokh <shayokhshuvro14@gmail.com>
2025-10-25 11:00:48 +0600

    add initial sign up page, install Tanstack Form

---
+++ app/signup/_components/SignUp.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import eye from "@/public/eye.svg";
+import eye_closed from "@/public/eye-closed.svg";
+import google from "@/public/google.svg";
+import logo from "@/public/logo.svg";
+
+export function SignUp() {
+  return (
+    <section className="flex flex-col items-center justify-center h-svh">
+      <div className="flex flex-col items-center max-w-64">
+        <Image src={logo} alt="logo" className="w-26" />
+
+        <p className="text-xl text-neutral-300 font-medium mt-[1rem]">
+          Create a new account
+        </p>
+
+        {/* action={handleGoogleAuth} */}
+        <form className="w-full">
+          <button
+            type="submit"
+            className="flex gap-x-3 justify-center items-center text-nowrap w-full bg-neutral-200 text-neutral-800 font-medium mt-6 py-2 rounded-sm hover:cursor-pointer select-none"
+          >
+            <Image src={google} alt="icon" className="size-5" />
+            <span className="text-sm">Continue with Google</span>
+          </button>
+        </form>
+        <div className="flex items-center gap-x-3 w-full mt-2">
+          <div className="h-[0.5px] w-full bg-neutral-800" />
+          <p className="text-neutral-500 text-sm text-nowrap">or use email</p>
+          <div className="h-[0.5px] w-full bg-neutral-800" />
+        </div>
+        <form
+          className="flex flex-col w-full gap-y-2 mt-2 text-sm"
+          //   onSubmit={handleSubmit(onCredentialSubmitLogic)}
+        >
+          <input
+            // {...register("name")}
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            className="px-4 py-2 rounded-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500"
+          />
+
+          {/* {errors.name && (
+            <p className="text-red-500 text-sm">{errors.name.message}</p>
+          )} */}
+
+          <input
+            // {...register("email")}
+            type="text"
+            name="email"
+            placeholder="john@example.com"
+            className="px-4 py-2 rounded-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500"
+          />
+
+          {/* {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email.message}</p>
+          )} */}
+
+          {/* <PasswordField errors={errors} register={register} /> */}
+
+          <input
+            // {...register("confirmPassword")}
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm your password"
+            className="px-4 py-2 rounded-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500 w-full"
+          />
+          {/* 
+          {errors.confirmPassword && (
+            <p className="text-red-500 text-sm">
+              {errors.confirmPassword.message}
+            </p>
+          )}
+
+          {errors.root && (
+            <p className="text-red-500 text-sm">{errors.root.message}</p>
+          )} */}
+
+          <button
+            type="submit"
+            // disabled={isSubmitting}
+            className="px-3 py-2 hover:cursor-pointer bg-neutral-800 hover:bg-neutral-700 rounded-lg text-sm text-neutral-200 font-medium transition-colors duration-150 disabled:cursor-not-allowed disabled:bg-neutral-900"
+          >
+            Placeholder
+            {/* {isSubmitting ? "Hashing..." : "Sign up"} */}
+          </button>
+        </form>
+        <p className="text-neutral-500 text-sm text-nowrap mt-4">
+          Already have an account?{" "}
+          <Link href="/login" className="text-neutral-300 underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+function PasswordField() {
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+
+  return (
+    <>
+      <div className="w-full flex">
+        <input
+          type={isPasswordShown ? "text" : "password"}
+          name="password"
+          placeholder="Enter your password"
+          className="px-4 py-2 rounded-l-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500 w-full"
+        />
+
+        <button
+          type="button"
+          className="p-2 border border-l-0 border-neutral-600 bg-neutral-800 rounded-r-lg hover:cursor-pointer"
+          onClick={() => setIsPasswordShown(!isPasswordShown)}
+        >
+          {/* biome-ignore lint/performance/noImgElement: <> */}
+          <img
+            src={isPasswordShown ? eye_closed : eye}
+            alt={isPasswordShown ? "Hide password" : "Show password"}
+          />
+
+          {isPasswordShown ? (
+            <span className="sr-only">Hide password</span>
+          ) : (
+            <span className="sr-only">Show password</span>
+          )}
+        </button>
+      </div>
+
+      {/* {errors.password && (
+        <p className="text-red-500 text-sm">{errors.password.message}</p>
+      )} */}
+    </>
+  );
+}
