74f76d604c54dee0e18c6608c979fa41f57b9f94
Shayokh <shayokhshuvro14@gmail.com>
2025-11-22 15:43:45 +0600

    install Tanstack query

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { auth } from "@/utils/auth";
@@ -11,11 +12,15 @@ export default async function Page() {
     redirect("/signin");
   }
 
+  const queryClient = new QueryClient();
+
   return (
-    <div className="h-svh py-2 pr-2">
-      <div className="h-full border border-neutral-200 rounded-xl">
-        <Home />
+    <QueryClientProvider client={queryClient}>
+      <div className="h-svh py-2 pr-2">
+        <div className="h-full border border-neutral-200 rounded-xl">
+          <Home />
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
